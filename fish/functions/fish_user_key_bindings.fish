e47158cd80ecaeb835d7d27cf180e3b691bd0110
JulesHauchecorne <youle21@yahoo.ca>
2021-11-15 23:34:29 -0500

    and the rest

---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,12 @@
+function fish_user_key_bindings
+    fish_vi_mode
+    bind -M $mode \cf accept-autosuggestion
+    bind \cf accept-autosuggestion
+end
+
+function fish_user_key_bindings
+bind \cr peco_select_history 
+bind \cf peco_change_directory 
+bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+bind -M insert kk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+end
